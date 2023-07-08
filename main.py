57e5c9315988aeedea545bb18c30d5252b913ca3
Chandana Muttur <120451891+mechandumuttur@users.noreply.github.com>
2023-07-08 11:55:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ while True:
         print('Soil is moist.')
 
     # Wait for 1 second
-    time.sleep(1)
+    time.sleep(1
+
+               #If the sensor value is 1000 or more than that then the sensor is not in the soil or sensor is disconnected. 
+               #If the sensor value is more than 600 but less than 1000 then the soil is dry. 
+               #If the sensor value is 370 to 600 then the soil is humid.
+   #If the sensor value is less than 370 then the sensor in the water.
    
 
